940154fc4db4d58721b78a3b2ac9d41d7589f6cd
Nastia Ochakovsky <staltica@gmail.com>
2018-03-22 19:46:00 +0200

    app changes

--- app.js
+++ app.js
@@ -8,17 +8,13 @@ var port = process.env.PORT || 1337;
 app.use(bodyParser.urlencoded({ extended: true }));
  
 // test route
-app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
+app.get('/', function (req, res) { res.status(200).send('server is up and rinning'); });
  
-app.listen(port, function () {
-  console.log('Listening on port ' + port);
-});
-
-
 app.post('/smart', function (req, res, next) {
     var userName = req.body.user_name;
     var userText = req.body.text;
-    var output; 
+    var output;
+
     if (userText == "why slack") {
         output = "1. knowledge management: uploading and searching of files (+ connection to external sources), code snippets, videos and more \n" +
 		"2. communication management: multiple channeles / workspaces, external messengers integration";
@@ -29,6 +25,7 @@ app.post('/smart', function (req, res, next) {
     var botPayload = {
         text: output
     };
+
   // Loop otherwise..
   if (userName !== 'slackbot') {
     return res.status(200).json(botPayload);
